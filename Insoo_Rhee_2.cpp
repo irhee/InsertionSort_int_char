677c72faf2b8125d2cf7fd5c97536368b73bd368
ira3 <insoorhee@hotmail.com>
2018-01-14 21:26:34 -0800

    InsertionSort

--- Insoo_Rhee_2.cpp
+++ Insoo_Rhee_2.cpp
@@ -3,8 +3,9 @@ Jan 20, 2018
 Insoo Rhee (301075548)
 Assignment1. Question2.
 *******************************************************************************/
-#include <stdio.h>
+#include <iostream>
 #define MAX 10
+using namespace std;
 char selection();
 void input_I(unsigned int *A, int *size);
 void InsertionSort_I(unsigned int *A, const int *size);
@@ -17,20 +18,20 @@ int main()
     if ( 'I' == selection())
     {
         int size = 0;
-        unsigned int *A = (unsigned int*)malloc(MAX*sizeof(unsigned int));      
+        unsigned int *A = new unsigned int;
         input_I(A,&size);
         InsertionSort_I(A,&size);
         print_I(A,&size);
-        free(A);
+        delete[] A;
     }
     else
     {
         int size = 0;
-        char *B = (char*)malloc(MAX*sizeof(char)); 
+        char *B = new char; 
         input_C(B,&size);
         InsertionSort_C(B,&size);
         print_C(B,&size);
-        free(B);
+        delete[] B;
     }
 
     return 0;
@@ -38,31 +39,31 @@ int main()
 char selection()
 {
     char c;
-    printf("Enter capital 'I' to sort integers or\n");
-    printf("Enter capital 'C' to sort characters: ");
-    scanf("%c",&c);
+    cout<<"Enter capital 'I' to sort integers or\n";
+    cout<<"Enter capital 'C' to sort characters: ";
+    cin>>c;
     return c;    
 }
 void input_I(unsigned int *A, int *size)
 {
-    unsigned int cin;
+    unsigned int input;
     int count = MAX;
     *size = 0;
     
-    printf("(%d inputs remaining)\n",count);
-    printf("Enter an unsigned integer(any alphabet to exit): ");    
-    while(scanf("%u",&cin) == 1 && count > 0)
+    cout<<count<<" inputs remaining"<<endl;
+    cout<<"Enter an unsigned integer(any alphabet to exit): ";    
+    while( cin>>input && count > 0)
     {
-        A[*size] = cin;
+        A[*size] = input;
         *size = *size + 1;
         count--;
         
-        printf("(%d inputs remaining)\n",count);
-        printf("Enter an unsigned integer(any alphabet to exit): ");
+        cout<<count<<" inputs remaining\n";
+        cout<<"Enter an unsigned integer(any alphabet to exit): ";
     }
     if(count == 0)
     {
-        printf("You reached ten inputs\n");
+        cout<<"You reached ten inputs\n";
     }
 }
 void InsertionSort_I(unsigned int *A, const int *size)
@@ -85,40 +86,38 @@ void InsertionSort_I(unsigned int *A, const int *size)
 void print_I(unsigned int *A, const int *size)
 {
     int i;
-    printf("The sorted numbers are:\n");
+    cout<<"The sorted numbers are:\n";
     for(i=0;i<*size;i++)
     {
-        printf("%u\n",A[i]);
+        cout<<A[i]<<endl;
     }
 }
-void input_C(char *B, int *size)
+void input_C(char *A, int *size)
 {
-    getchar();
-    char cin;
+    char input;
     int count = MAX;
     *size = 0;
     do
     {
-        printf("(%d inputs remaining)\n",count);
-        printf("Enter a character(0 to exit): ");
-        scanf("%c",&cin);
-        getchar();
+        cout<<count<<" inputs remaining\n";
+        cout<<"Enter a character(0 to exit): ";
+        cin>>input;
         
-        if(cin != '0')
+        if(input != '0')
         {
-            B[*size] = cin;
+            A[*size] = input;
             *size = *size + 1;
         }
         count--;
-    }while(cin != '0' && count > 0);
+    }while(input != '0' && count > 0);
     if(count == 0)
     {
-        printf("You have ten inputs\n");
+        cout<<"You have ten inputs\n";
     }
 }
 void InsertionSort_C(char *A, const int *size)
 {
-    printf("The sorted characters are:\n");
+    cout<<"The sorted characters are:\n";
     int i;
     int j;
     char cur;
@@ -139,6 +138,6 @@ void print_C(char *A, const int *size)
     int i;
     for(i=0;i<*size;i++)
     {
-        printf("%c\n",A[i]);
+        cout<<A[i]<<"\n";
     }
 }
